46d4cc1c0ba1781377ac642158dba0f5d5df087f
Cuetotec <cuetoburgos@gmail.com>
2026-03-12 15:15:19 +0100

    Entregas Proyecto

--- JavaFX/AdminController.java
+++ JavaFX/AdminController.java
@@ -10,14 +10,15 @@ public class AdminController {
     @FXML
     private Label lblStockAlerta;
 
-    // Método para actualizar el estado del pedido desde el panel [cite: 296, 303]
+    // Método para actualizar el estado del pedido desde el panel
     @FXML
     public void finalizarReparacion() {
         Pedido seleccionado = tablaPedidos.getSelectionModel().getSelectedItem();
         if (seleccionado != null) {
-            seleccionado.setEstado("Terminado"); // [cite: 135]
+            seleccionado.setEstado("Terminado");
             // Aquí iría la llamada al backend para actualizar la DB
-            System.out.println("Pedido " + seleccionado.getIdPedido() + " actualizado a Terminado.");
+            System.out.println("Pedido " + seleccionado.getIdPedido() + 
+            " actualizado a Terminado.");
         }
     }
 }
